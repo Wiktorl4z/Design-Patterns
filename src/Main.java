587fe49f0d2a7a67f454392682628bbfb61a5f45
Wiktor Kalinowski <wiktorkalinowski@gmail.com>
2019-07-11 00:43:31 +0200

    Builder classic

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,29 @@
+import house.BigHouseBuilder;
+import house.House;
+import house.HouseDirector;
+import house.SmallHouseBuilder;
+
 public class Main {
 
     public static void main(String[] args) {
-        FlightLeg leg = new FlightLeg.FlightLegBuilder("Las Vegas", "Los Angeles").price(50).build();
-        System.out.println(leg);
 
-  /*      FlightLeg leg = new FlightLeg.FlightLegBuilder("Las Vegas","Los Angeles").build();
-        System.out.println(leg);*/
+//        House house1 = new House("walls", "floors", "rooms", "windows", "doors", "garage");
+//        House house2 = new House("")
+
+        SmallHouseBuilder smallHouseBuilder = new SmallHouseBuilder();
+        BigHouseBuilder bigHouseBuilder = new BigHouseBuilder();
+
+        HouseDirector smallHouseDirector = new HouseDirector(smallHouseBuilder);
+        smallHouseDirector.buildHouse();
+
+        HouseDirector bigHouseDirector = new HouseDirector(bigHouseBuilder);
+        bigHouseDirector.buildHouse();
+
+        House smallHouse = smallHouseDirector.getHouse();
+        House bigHouse = bigHouseDirector.getHouse();
+
+        System.out.println(smallHouse);
+        System.out.println(bigHouse);
 
     }
 }
---
+++ src/house/HouseBuilder.java
@@ -0,0 +1,15 @@
+package house;
+
+public interface HouseBuilder {
+
+    void buildWalls();
+    void buildFloors();
+    void buildRooms();
+    void buildRoof();
+    void buildWindows();
+    void biuldDoors();
+    void buildGarage();
+
+    House getHouse();
+
+}
